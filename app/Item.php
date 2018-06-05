b0883bb2fe98c728a18c442ea528ead2663f7d93
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-05 09:37:10 +0000

    model created for the items table

---
+++ app/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+     //
+    //Table Name
+    protected $table ="items";
+    //Primary Key
+    public $primaryKey='id';
+   //Timestamp
+   public $timestamp =true;
+}
