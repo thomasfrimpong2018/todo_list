72a8ad52a02a0de26c5c47ed16752a2f9d3e9b3d
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-05 13:22:07 +0000

    a home page to display users items with checkbox to verify done status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,86 +1,41 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <style>
-                    html, body {
-                        background-color: #fff;
-                        color: #636b6f;
-                        font-family: 'Raleway', sans-serif;
-                        font-weight: 100;
-                        height: 100vh;
-                        margin: 0;
-                    }
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-                    .full-height {
-                        height: 100vh;
-                    }
-
-                    .flex-center {
-                        align-items: center;
-                        display: flex;
-                        justify-content: center;
-                    }
-
-                    .position-ref {
-                        position: relative;
-                    }
-
-                    .top-right {
-                        position: absolute;
-                        right: 10px;
-                        top: 18px;
-                    }
-
-                    .content {
-                        text-align: center;
-                    }
-
-                    .title {
-                        font-size: 84px;
-                    }
-
-                    .links > a {
-                        color: #636b6f;
-                        padding: 0 25px;
-                        font-size: 12px;
-                        font-weight: 600;
-                        letter-spacing: .1rem;
-                        text-decoration: none;
-                        text-transform: uppercase;
-                    }
-
-                    .m-b-md {
-                        margin-bottom: 30px;
-                    }
-                </style>
-
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+                    You are logged in!
                 </div>
-            @endif
+                <div class="container">
+                   <h2>Your Items</h2><hr>
+                   <ul>
+                   @foreach($items as $item)
+                     <li>
+                      {!!Form::open(['action'=>'HomeController@postIndex','method'=>'POST'])!!}
+                      
+                     <input type="checkbox" 
+                     onClick="this.form.submit()"
+                      {{$item->done ? 'checked':''}} />
 
-            <div class="content">
-                
-                   @yield('content')
-
+                     <input  type="hidden" name="id" value="{{$item->id}}" />
+                     {{$item->name}}
+                      {!!Form::close()!!}
+                     </li>
+                    @endforeach
+                   </ul>
+                </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
