622537a905069d4c297623240596f240e8311649
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-05 10:06:32 +0000

    function created to display users dashboard with the  items he created

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
-    public function index(){
-      return view('index');
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items=auth()->user()->items;
+        return view('home')->with('items',$items);
     }
 }
