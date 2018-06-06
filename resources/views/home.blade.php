40ed5b9e900511d3cc1dc6c1f5e61454a5a4605d
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-06 10:44:01 +0000

    display messages are added to tweak the user experience

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,12 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    
                 </div>
                 <div class="container">
-                   <h2>Your Items</h2><small><a href="/new">Create New Task</a></small><hr>
+                <h2>Your Items</h2><small><a href="/new">Create New Task</a></small><hr>
+                 @if(count($items )>0)
+                   <small><b>Click the checkbox to mark the task as done</b></small><br><br>
                    <ul>
                    @foreach($items as $item)
                      <li>
@@ -30,9 +32,20 @@
                      <input  type="hidden" name="id" value="{{$item->id}}" />
                      {{$item->name}}
                       {!!Form::close()!!}
+                     
+                     {!!Form::open(['action'=>['HomeController@deleteTask',$item->id],'method'=>'POST'])!!}
+                     <!--<button class="btn btn-danger" onClick="this.form.submit()" >x</button>-->
+                     <input  type="hidden" name="id" value="{{$item->id}}" />
+                     {{Form::hidden('_method','DELETE')}}
+                     {{Form::submit('Delete Task',['class'=>'btn btn-danger'])}}
+                    {!!Form::close()!!}<br>
+                     
                      </li>
                     @endforeach
                    </ul>
+                   @else
+                   <h6>No Task Created Yet</h6>
+                   @endif
                 </div>
             </div>
         </div>
